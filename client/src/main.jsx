8585f046b68dc02975185e55ada73eb9da716abf
PranavHarane <pranavharne00@gmail.com>
2026-06-11 17:55:57 +0530

    uninstalled some extra packages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import './input.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import FlashMessageContext from './context/FlashMessageContext.jsx'
+import FlashMessageContext from './context/flashMessageContext.jsx'
 import { Bounce, ToastContainer } from 'react-toastify'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
